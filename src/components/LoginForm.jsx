413e3e70c2d85d1acbdf67fe5509177671f0509f
Neeraj Singh Rajput <78468513+NeerajSingh007@users.noreply.github.com>
2022-06-16 17:27:27 +0530

    commit

---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,50 @@
+import {useState} from 'react'
+import {AuthContext} from "./AuthContext";
+import {useContext} from 'react'
+function LoginForm(){
+    const {handleToken} =useContext(AuthContext)
+
+    const [form, setForm]= useState({
+        email:"",
+        password:""
+    });
+    
+    const handlechange=(e)=>{
+        const {name, value}= e.target;
+        setForm ({...form, [name]:value})
+    }
+    
+    const handleSubmit= async(e)=>{
+        e.preventDefault();
+        let logindata=form;
+        let login_data= JSON.stringify(logindata);
+        let res= await fetch("https://reqres.in/api/login",
+        {
+            method:"POST",
+            body:login_data,
+            headers:
+            {"Content-type":"application/json"}
+        });
+        let data=await res.json();
+        if(data.token){
+            handleToken(data.token)
+        }
+        else
+        {
+            alert("Email or Passowrd is invalid")
+        }
+        
+
+    }
+    // "email": "eve.holt@reqres.in",
+    // "password": "cityslicka"
+
+    return(
+        <form onSubmit={handleSubmit} className="login">
+            <input name='email' onChange={handlechange} type="email" placeholder="enter email"></input>
+            <input name='password' onChange={handlechange} type="password" placeholder="enter password"></input>
+            <input name='submit'  type="submit" value="submit"></input> 
+        </form>
+    )
+}
+export default LoginForm;
